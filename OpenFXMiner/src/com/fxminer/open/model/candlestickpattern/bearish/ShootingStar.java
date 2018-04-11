009241c1181eb380564d649a69865d5e0a3d3b77
Andrew Kreimer <algonell@gmail.com>
2018-04-11 12:16:46 +0300

    ShootingStar

---
+++ OpenFXMiner/src/com/fxminer/open/model/candlestickpattern/bearish/ShootingStar.java
@@ -0,0 +1,33 @@
+package com.fxminer.open.model.candlestickpattern.bearish;
+
+import com.fxminer.open.model.Quote;
+import com.fxminer.open.model.Symbol;
+
+/**
+ * Prerequisites:
+ * 1. Preceding uptrend
+ * 2. Little/no lower shadow
+ * 3. Upper shadow at least 2x longer than real body
+ * 
+ * @author Andrew Kreimer
+ *
+ */
+public class ShootingStar extends BearishPattern {
+
+	@Override
+	public boolean isPresent(Symbol symbol) {
+		Quote q = symbol.getHistory().get(0);
+		double body = q.getOpen() - q.getAdjClose();
+		
+		return q.getOpen() > q.getAdjClose() && 
+				q.getAdjClose() == q.getLow() &&
+				q.getOpen() < q.getHigh() &&
+				body * 2 <= (q.getHigh() - q.getOpen());
+	}
+
+	@Override
+	public String getName() {
+		return "ShootingStar";
+	}
+
+}
