8f5a9509ec696bb220b2f2d35000faf3a3215688
Andrew Kreimer <algonell@gmail.com>
2018-05-02 23:07:58 +0300

    added change and % change

--- OpenFXMiner/src/com/fxminer/open/model/Price.java
+++ OpenFXMiner/src/com/fxminer/open/model/Price.java
@@ -10,6 +10,7 @@ public class Price {
 	private double bid;
 	private double ask;
 	private long volume;
+	private double close;
 
 	public double getBid() {
 		return bid;
@@ -34,17 +35,33 @@ public class Price {
 	public void setVolume(long volume) {
 		this.volume = volume;
 	}
-	
+
+	public double getClose() {
+		return close;
+	}
+
+	public void setClose(double close) {
+		this.close = close;
+	}
+
 	public double getMid() {
 		return weka.core.Utils.roundDouble((ask + bid) / 2, 2);
 	}
-	
+
 	public double getSpread() {
 		return weka.core.Utils.roundDouble(ask - bid, 2);
 	}
 
+	public double getChange() {
+		return weka.core.Utils.roundDouble(getMid() - close, 2); 
+	}
+	
+	public double getChangePercent() {
+		return weka.core.Utils.roundDouble(getChange() / close * 100, 2); 
+	}
+	
 	@Override
 	public String toString() {
-		return "[" + getMid() + ", " + getSpread() + ", " + volume + "]";
+		return "[" + getMid() + ", " + getChange() + ", " + getChangePercent() + ", " + getSpread() + ", " + volume + "]";
 	}
 }
