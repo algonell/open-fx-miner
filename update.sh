7fa22925b873056adbe7076f04f14306d4b1c3f2
void <algonell@gmail.com>
2024-02-23 20:29:14 +0200

    chore: avoid backup creation on update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Maven..."
-mvn -f ~/git/open-fx-miner versions:use-latest-releases
+mvn -f ~/git/open-fx-miner versions:use-latest-releases -DgenerateBackupPoms=false
 
 echo "build..."
 ~/git/open-fx-miner/build.sh
