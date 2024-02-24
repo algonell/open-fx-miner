2437edc7f4d713ae34f2b1fc69da08811aedc07e
void <algonell@gmail.com>
2024-02-24 11:41:50 +0200

    style: format

--- format.sh
+++ format.sh
@@ -2,10 +2,10 @@
 
 clear
 
+echo
 echo "google-java-format..."
 java -jar ~/Misc/google-java-format-*.jar --replace $(git ls-files ~/git/open-fx-miner/*.java)
 
 echo
-
 echo "SpotBugs..."
 mvn -f ~/git/open-fx-miner spotbugs:check
