d601ae5156dd28017335884abec31923605a7c59
void <algonell@gmail.com>
2024-02-24 12:11:12 +0200

    feat: format XML on update

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 echo "Maven..."
 mvn -f ~/git/open-fx-miner versions:use-latest-releases -DgenerateBackupPoms=false
+find ~/git/open-fx-miner -name "*.xml" -exec xmllint --format '{}' --output '{}' \;
 
 echo "build..."
 ~/git/open-fx-miner/build.sh
